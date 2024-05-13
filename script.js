052b10003a73b9ff8924352240a1a881c9cd163e
Ehonam <164899950+Ehonam@users.noreply.github.com>
2024-05-13 17:03:10 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,98 @@
+
+/* Exercise 1 CSS */
+
+#square {
+    height: 200px;
+    width: 200px;
+    background-color: rgb(41, 182, 123);
+    display: flex;
+    justify-content: center;
+    position: relative;
+    margin-top: 100px;
+    left: 40%;
+    vertical-align: middle;
+    align-items: center;
+    gap: 10px;
+
+}
+
+.text {
+    justify-content: center;
+    position: absolute;
+    vertical-align: middle;
+    display: flex;
+    flex-direction: column;
+}
+
+h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: xx-large;
+    color: azure;
+    text-align: center;
+}
+
+h1:hover {
+    color: rgb(141, 93, 4);
+}
+
+
+/* Exercise 2 CSS */
+
+.line1 {
+    display: flex;
+    justify-content: center;
+    position: relative;
+    margin-top: 100px;
+    vertical-align: middle;
+    align-items: center;
+    gap: 10px;
+}
+
+.squarebox {
+    height: 200px;
+    width: 200px;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    vertical-align: middle;
+    align-items: center;
+}
+
+
+.line1 {
+    display: flex;
+    justify-content: center;
+    position: relative;
+    margin-top: 100px;
+    vertical-align: middle;
+    align-items: center;
+    margin-top: 100px;
+}
+
+.line2 {
+    display: flex;
+    justify-content: center;
+    position: relative;
+    vertical-align: middle;
+    align-items: center;
+    margin-top: 10px}
+
+.one {
+    background-color: red;
+}
+
+.two {
+    background-color: blue;
+}
+
+.three {
+    background-color: green;
+}       
+
+.four {       
+    background-color: yellow;   
+}   
+
+.selection {
+    background-color: red;
+}
